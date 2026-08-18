3e7ac09650dbb3bb7941886463f073156be4361a
ddai09-dot <ddai09@yahoo.co.jp>
2026-08-18 10:09:57 +0900

    Task 20-D2I: remove unused Drift import

--- tools/task20_d2i_local_reset_prepare_test.dart
+++ tools/task20_d2i_local_reset_prepare_test.dart
@@ -2,7 +2,6 @@
 
 import 'dart:convert';
 
-import 'package:drift/drift.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
