a57f96e6a4e93e5a682324f6ace47df71a99904b
ddai09-dot <ddai09@yahoo.co.jp>
2026-09-24 10:20:56 +0900

    Task 20-D2M: fix Drift matcher import ambiguity

--- tools/task20_d2m_rest_day_rollover_verify_test.dart
+++ tools/task20_d2m_rest_day_rollover_verify_test.dart
@@ -2,7 +2,7 @@
 
 import 'dart:convert';
 
-import 'package:drift/drift.dart';
+import 'package:drift/drift.dart' hide isNull, isNotNull;
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
